083dfa7bc7ef235f41a04952274375eebf7d059d
sonnguyen123526 <nguyenso@dickinson.edu>
2026-01-28 14:24:30 -0500

    update file structure

--- translation/config/basic.py
+++ translation/config/basic.py
@@ -42,6 +42,6 @@ compile = False # do not torch compile the model
 # start = "\n" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
 # num_samples = 10 # number of samples to draw
 # max_new_tokens = 500 # number of tokens generated in each sample
-temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
+temperature = 0.3 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
 # top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
 # seed = 1337
